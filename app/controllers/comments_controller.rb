929ae2a42b8266cc082d52a03d7065d1cbcc29b0
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-04 11:19:03 +0900

    コメント修正後

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
   def create
-   @comment = Comment.new(comment_params)
-   if @comment.save
-    redirect_to prototype_path(@comment.prototype)
-  else
-    @prototype = @comment.prototype
-    @comments = @prototype.comments
-    render "prototypes/show"
-  end 
+    @prototype = Prototype.find(params[:prototype_id]) # プロトタイプを取得
+    @comment = @prototype.comments.build(comment_params) # プロトタイプに紐づいたコメントを作成
+    @comment.user = current_user # コメントのユーザーを設定
+  
+    if @comment.save
+      redirect_to prototype_path(@prototype), notice: "コメントを投稿しました"
+    else
+      @comments = @prototype.comments.includes(:user) # 既存のコメントを取得
+      render "prototypes/show", status: :unprocessable_entity
+    end
   end
 
   private
+
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id:current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
 end
